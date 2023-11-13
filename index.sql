18b6924adf141e4f7a288a68f3834c7c5d0fda6a
Bernardo Mendes <134334489+brnd09@users.noreply.github.com>
2023-11-13 15:56:08 -0300

    Update index.sql
    update

--- index.sql
+++ index.sql
@@ -1,26 +1,273 @@
-create schema fortes;
-show databases;
-use fortes;
+-- MySQL Workbench Forward Engineering
 
-create table funcionarios(
-cpf varchar(14) unique primary key not null,
-matricula int unique not null,
-nome varchar(100) unique not null,
-id_card int unique not null,
-cargo varchar(50) not null
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-create table fornecedores(
-cnpj varchar(18) unique primary key not null,
-razão_social varchar(50),
-cep int not null,
-complemento varchar(100) not null,
-refefencia varchar(100) 
-);
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
 
-create table obras(
-id_obra int unique primary key not null,
-id_funcionario_obra int not null,
-constraint id_funcionario_obra foreign key(id_funcionario_obra) references funcionarios(cpf),
-cep int not null
-);
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`cargo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`cargo` (
+  `idcargo` INT NOT NULL,
+  `nome_cargo` VARCHAR(45) NOT NULL,
+  `responsa` TEXT(255) NULL,
+  `salario` DECIMAL(10,2) NOT NULL,
+  `beneficio` TEXT(255) NULL,
+  UNIQUE INDEX `idcargo_UNIQUE` (`idcargo` ASC) VISIBLE,
+  PRIMARY KEY (`idcargo`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`func`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`func` (
+  `idfunc` INT NOT NULL,
+  `cpf` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `cargo_idcargo` INT NOT NULL,
+  PRIMARY KEY (`idfunc`, `cargo_idcargo`),
+  UNIQUE INDEX `idfunc_UNIQUE` (`idfunc` ASC) VISIBLE,
+  UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC) VISIBLE,
+  INDEX `fk_func_cargo_idx` (`cargo_idcargo` ASC) VISIBLE,
+  CONSTRAINT `fk_func_cargo`
+    FOREIGN KEY (`cargo_idcargo`)
+    REFERENCES `mydb`.`cargo` (`idcargo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`endereço`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`endereço` (
+  `cep` INT NOT NULL,
+  `cidade` VARCHAR(45) NOT NULL,
+  `estado` VARCHAR(45) NOT NULL,
+  `logradouro` VARCHAR(45) NOT NULL,
+  UNIQUE INDEX `cep_UNIQUE` (`cep` ASC) VISIBLE,
+  PRIMARY KEY (`cep`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`fornecedor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`fornecedor` (
+  `cnpj` INT NOT NULL,
+  `razao_social` VARCHAR(45) NOT NULL,
+  `fornecedorcol` VARCHAR(45) NULL,
+  `id_endereço` INT NOT NULL,
+  PRIMARY KEY (`cnpj`, `id_endereço`),
+  UNIQUE INDEX `idfornecedor_UNIQUE` (`cnpj` ASC) VISIBLE,
+  UNIQUE INDEX `razao_social_UNIQUE` (`razao_social` ASC) VISIBLE,
+  INDEX `fk_fornecedor_endereço1_idx` (`id_endereço` ASC) VISIBLE,
+  CONSTRAINT `fk_fornecedor_endereço1`
+    FOREIGN KEY (`id_endereço`)
+    REFERENCES `mydb`.`endereço` (`cep`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`endereco_obra`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`endereco_obra` (
+  `id_end_obra` INT NOT NULL,
+  `cep` INT NOT NULL,
+  `complemento` VARCHAR(45) NULL,
+  `logradouro` VARCHAR(45) NOT NULL,
+  UNIQUE INDEX `id_end_obra_UNIQUE` (`id_end_obra` ASC) VISIBLE,
+  PRIMARY KEY (`id_end_obra`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`obras`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`obras` (
+  `idobras` INT NOT NULL,
+  `status` VARCHAR(45) NOT NULL,
+  `id_end_obra` INT NOT NULL,
+  PRIMARY KEY (`idobras`),
+  UNIQUE INDEX `idobras_UNIQUE` (`idobras` ASC) VISIBLE,
+  INDEX `fk_obras_endereco_obra1_idx` (`id_end_obra` ASC) VISIBLE,
+  CONSTRAINT `fk_obras_endereco_obra1`
+    FOREIGN KEY (`id_end_obra`)
+    REFERENCES `mydb`.`endereco_obra` (`id_end_obra`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`func_obra`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`func_obra` (
+  `id_func_obra` INT NOT NULL,
+  `func_idfunc` INT NOT NULL,
+  `obras_idobras` INT NOT NULL,
+  PRIMARY KEY (`id_func_obra`),
+  UNIQUE INDEX `id_func_obra_UNIQUE` (`id_func_obra` ASC) VISIBLE,
+  INDEX `fk_func_obra_func1_idx` (`func_idfunc` ASC) VISIBLE,
+  INDEX `fk_func_obra_obras1_idx` (`obras_idobras` ASC) VISIBLE,
+  UNIQUE INDEX `func_idfunc_UNIQUE` (`func_idfunc` ASC) VISIBLE,
+  UNIQUE INDEX `obras_idobras_UNIQUE` (`obras_idobras` ASC) VISIBLE,
+  CONSTRAINT `fk_func_obra_func1`
+    FOREIGN KEY (`func_idfunc`)
+    REFERENCES `mydb`.`func` (`idfunc`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_func_obra_obras1`
+    FOREIGN KEY (`obras_idobras`)
+    REFERENCES `mydb`.`obras` (`idobras`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`refeicoes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`refeicoes` (
+  `id_refeicao` INT NOT NULL,
+  `tipo` VARCHAR(45) NOT NULL,
+  `valor_unitario` DECIMAL(10,2) NULL,
+  PRIMARY KEY (`id_refeicao`),
+  UNIQUE INDEX `idrefeicoes_UNIQUE` (`id_refeicao` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`programa_diario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`programa_diario` (
+  `id_programa` INT NOT NULL,
+  `data` DATE NOT NULL,
+  `tipo_dia` VARCHAR(45) NOT NULL,
+  `id_refeicao` INT NOT NULL,
+  `id_obras` INT NOT NULL,
+  PRIMARY KEY (`id_programa`, `id_obras`),
+  UNIQUE INDEX `id_programa_UNIQUE` (`id_programa` ASC) VISIBLE,
+  INDEX `fk_programa_diario_refeicoes1_idx` (`id_refeicao` ASC) VISIBLE,
+  INDEX `fk_programa_diario_obras1_idx` (`id_obras` ASC) VISIBLE,
+  CONSTRAINT `fk_programa_diario_refeicoes1`
+    FOREIGN KEY (`id_refeicao`)
+    REFERENCES `mydb`.`refeicoes` (`id_refeicao`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_programa_diario_obras1`
+    FOREIGN KEY (`id_obras`)
+    REFERENCES `mydb`.`obras` (`idobras`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`equipes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`equipes` (
+  `id_equipe` VARCHAR(45) NOT NULL,
+  `id_func` INT NOT NULL,
+  `id_obras` INT NOT NULL,
+  `cargo_equipe` VARCHAR(45) NULL,
+  PRIMARY KEY (`id_equipe`),
+  INDEX `fk_func_has_obras_obras1_idx` (`id_obras` ASC) VISIBLE,
+  INDEX `fk_func_has_obras_func1_idx` (`id_func` ASC) VISIBLE,
+  UNIQUE INDEX `id_equipe_UNIQUE` (`id_equipe` ASC) VISIBLE,
+  CONSTRAINT `fk_func_has_obras_func1`
+    FOREIGN KEY (`id_func`)
+    REFERENCES `mydb`.`func` (`idfunc`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_func_has_obras_obras1`
+    FOREIGN KEY (`id_obras`)
+    REFERENCES `mydb`.`obras` (`idobras`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`consumo_refeicoes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`consumo_refeicoes` (
+  `id_consumo` INT NOT NULL,
+  `id_func` INT NOT NULL,
+  `quant_consumida` VARCHAR(45) NULL,
+  `programa_diario_id_programa` INT NOT NULL,
+  PRIMARY KEY (`id_consumo`),
+  UNIQUE INDEX `id_cons_ref_UNIQUE` (`id_consumo` ASC) VISIBLE,
+  INDEX `fk_consumo_refeicoes_func1_idx` (`id_func` ASC) VISIBLE,
+  INDEX `fk_consumo_refeicoes_programa_diario1_idx` (`programa_diario_id_programa` ASC) VISIBLE,
+  CONSTRAINT `fk_consumo_refeicoes_func1`
+    FOREIGN KEY (`id_func`)
+    REFERENCES `mydb`.`func` (`idfunc`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_consumo_refeicoes_programa_diario1`
+    FOREIGN KEY (`programa_diario_id_programa`)
+    REFERENCES `mydb`.`programa_diario` (`id_programa`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`medicoes_pagamento`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`medicoes_pagamento` (
+  `id_medicao` INT NOT NULL,
+  `programa_diario_id_programa` INT NOT NULL,
+  `valor total` DECIMAL(10,2) NULL,
+  `quantidade_consumida_total` INT(45) NULL,
+  `pago` CHAR(1) NULL,
+  PRIMARY KEY (`id_medicao`),
+  UNIQUE INDEX `id_medicao_UNIQUE` (`id_medicao` ASC) VISIBLE,
+  INDEX `fk_medicoes_pagamento_programa_diario1_idx` (`programa_diario_id_programa` ASC) VISIBLE,
+  CONSTRAINT `fk_medicoes_pagamento_programa_diario1`
+    FOREIGN KEY (`programa_diario_id_programa`)
+    REFERENCES `mydb`.`programa_diario` (`id_programa`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`medicoes_pagamento_has_fornecedor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`medicoes_pagamento_has_fornecedor` (
+  `medicao_fornecedor` INT NOT NULL,
+  `fornecedor_cnpj` INT NOT NULL,
+  INDEX `fk_medicoes_pagamento_has_fornecedor_fornecedor1_idx` (`fornecedor_cnpj` ASC) VISIBLE,
+  INDEX `fk_medicoes_pagamento_has_fornecedor_medicoes_pagamento1_idx` (`medicao_fornecedor` ASC) VISIBLE,
+  PRIMARY KEY (`medicao_fornecedor`),
+  CONSTRAINT `fk_medicoes_pagamento_has_fornecedor_medicoes_pagamento1`
+    FOREIGN KEY (`medicao_fornecedor`)
+    REFERENCES `mydb`.`medicoes_pagamento` (`id_medicao`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_medicoes_pagamento_has_fornecedor_fornecedor1`
+    FOREIGN KEY (`fornecedor_cnpj`)
+    REFERENCES `mydb`.`fornecedor` (`cnpj`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
